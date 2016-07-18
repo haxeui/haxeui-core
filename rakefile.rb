98fffb1b05485e05ce59cc402db0091b9d0842c7
Ian Harrigan <ianharrigan@hotmail.com>
2016-07-18 18:17:57 +0200

    Add default rake task

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,12 @@
 require 'travis'
+require "bundler/gem_tasks"
+require 'rspec/core/rake_task'
+
+RSpec::Core::RakeTask.new
+task :default => :spec
 
 token = ENV['GH_TOKEN']
 
 # against the Travis namespace
 Travis.github_auth(token)
-puts "Hello #{Travis::User.current.name}!"
+puts "Using #{Travis::User.current.name}!"
