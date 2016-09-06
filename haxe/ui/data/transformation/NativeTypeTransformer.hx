b7fe09ade07e28650b730d1556cd79c487473895
Ian Harrigan <ianharrigan@hotmail.com>
2016-09-06 21:10:02 +0200

    change initial value to null

--- haxe/ui/data/transformation/NativeTypeTransformer.hx
+++ haxe/ui/data/transformation/NativeTypeTransformer.hx
@@ -5,7 +5,7 @@ class NativeTypeTransformer implements IItemTransformer<Dynamic> {
     }
     
     public function transformFrom(i:Dynamic):Dynamic {
-        var o:Dynamic = i;
+        var o:Dynamic = null;
         if (Std.is(i, String)) {
             o = { text: i, value: i };
         } else if (Std.is(i, Int) || Std.is(i, Float) || Std.is(i, Bool)) {
