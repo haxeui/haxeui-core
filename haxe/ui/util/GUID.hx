f533826a31bb7acb5eb72f1ab83acc0e5a1420da
Ian Harrigan <ianharrigan@hotmail.com>
2016-12-17 01:47:30 +0100

    fix guid

--- haxe/ui/util/GUID.hx
+++ haxe/ui/util/GUID.hx
@@ -10,7 +10,7 @@ class GUID {
                           "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d",
                           "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
                           "y", "z"];
-        var id:Array<String> = new [];
+        var id:Array<String> = [];
         radix = (radix > 61) ? 61 : radix;
 
         while (length-- > 0) {
