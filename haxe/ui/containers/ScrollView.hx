47fea3bc3e4286646abf921c8a5412ee601ed897
Ian Harrigan <ianharrigan@hotmail.com>
2017-01-30 22:14:03 +0100

    dont cancel event if not possible to scroll

--- haxe/ui/containers/ScrollView.hx
+++ haxe/ui/containers/ScrollView.hx
@@ -302,6 +302,10 @@ class ScrollView extends Component implements IClonable<ScrollView> {
     private var _inertiaDirectionY:Int;
     
     private function _onMouseDown(event:MouseEvent) {
+        if ((_hscroll == null || _hscroll.hidden == true) && (_vscroll == null || _vscroll.hidden == true)) {
+            return;
+        }
+        
         event.cancel();
         if (_hscroll != null && _hscroll.hidden == false && _hscroll.hitTest(event.screenX, event.screenY) == true) {
             return;
