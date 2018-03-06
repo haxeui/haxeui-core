935dc86dd749327ec2a07ceea0e01750834bab54
Ian Harrigan <ianharrigan@hotmail.com>
2018-03-06 22:18:07 +0100

    change default dimenions

--- haxe/ui/components/VerticalProgress2.hx
+++ haxe/ui/components/VerticalProgress2.hx
@@ -6,11 +6,11 @@ class VerticalProgress2 extends Progress2 {
     //***********************************************************************************************************
     private override function createChildren() { // TODO: this should be min-width / min-height in theme css when the new css engine is done
         super.createChildren();
-        if (width <= 0) {
-            width = 20;
+        if (componentWidth <= 0) {
+            componentWidth = 20;
         }
-        if (height <= 0) {
-            height = 150;
+        if (componentHeight <= 0) {
+            componentHeight = 150;
         }
     }
     
