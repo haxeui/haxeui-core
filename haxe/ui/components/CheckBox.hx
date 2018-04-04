3ea76c42112db127bb9e6a0598b6fe0450da3afd
Ian Harrigan <ianharrigan@hotmail.com>
2018-04-04 17:03:28 +0200

    make new versions default

--- haxe/ui/components/CheckBox.hx
+++ haxe/ui/components/CheckBox.hx
@@ -1,94 +1,53 @@
 package haxe.ui.components;
 
-import haxe.ui.validation.InvalidationFlags;
-import haxe.ui.core.Behaviour;
+import haxe.ui.core.Component;
+import haxe.ui.core.DataBehaviour;
 import haxe.ui.core.InteractiveComponent;
 import haxe.ui.core.MouseEvent;
 import haxe.ui.core.UIEvent;
 import haxe.ui.layouts.HorizontalLayout;
 import haxe.ui.styles.Style;
-import haxe.ui.util.Variant;
 
-/**
- Checkbox component showing either a selected or unselected state including a text label
-**/
-@:dox(icon = "/icons/ui-check-boxes.png")
 class CheckBox extends InteractiveComponent {
-    public function new() {
-        super();
-    }
+    //***********************************************************************************************************
+    // Public API
+    //***********************************************************************************************************
+    @:behaviour(TextBehaviour)              public var text:String;
+    @:behaviour(SelectedBehaviour)          public var selected:Bool;
 
     //***********************************************************************************************************
     // Internals
     //***********************************************************************************************************
-    private override function createDefaults() {
+    private override function createDefaults() {  // TODO: remove this eventually, @:layout(...) or something
         super.createDefaults();
-        defaultBehaviours([
-            "text" => new CheckBoxDefaultTextBehaviour(this),
-            "selected" => new CheckBoxDefaultSelectedBehaviour(this)
-        ]);
         _defaultLayout = new HorizontalLayout();
     }
 
     private override function createChildren() {
-        var checkboxValue:CheckBoxValue = findComponent(CheckBoxValue);
-        if (checkboxValue == null) {
-            checkboxValue = new CheckBoxValue();
-            checkboxValue.id = "checkbox-value";
-            checkboxValue.addClass("checkbox-value");
-            addComponent(checkboxValue);
-            
-            checkboxValue.registerEvent(MouseEvent.CLICK, _onClick);
-            checkboxValue.registerEvent(MouseEvent.MOUSE_OVER, _onMouseOver);
-            checkboxValue.registerEvent(MouseEvent.MOUSE_OUT, _onMouseOut);
+        super.createChildren();
+        
+        if (findComponent(Value) == null) {
+            var value = new Value();
+            value.id = '${cssName}-value';
+            value.addClass('${cssName}-value');
+            value.scriptAccess = false;
+            addComponent(value);
         }
+        
+        registerInternalEvents(Events);
     }
-
-    private override function destroyChildren() {
-        var value:CheckBoxValue = findComponent(CheckBoxValue);
-        if (value != null) {
-            removeComponent(value);
-            value = null;
-        }
-
-        var label:Label = findComponent(Label);
-        if (label != null) {
-            removeComponent(label);
-            label = null;
-        }
-    }
-
+    
     //***********************************************************************************************************
     // Overrides
     //***********************************************************************************************************
-    private override function get_value():Variant {
-        return selected;
-    }
-
-    private override function set_value(value:Variant):Variant {
-        selected = value;
-        return value;
-    }
-
-    private override function set_text(value:String):String {
-        if (_text == value) {
-            return value;
-        }
-
-        invalidateData();
-        _text = value;
-        return value;
-    }
-
-    private override function applyStyle(style:Style) {
+    private override function applyStyle(style:Style) {  // TODO: remove this eventually, @:styleApplier(...) or something
         super.applyStyle(style);
-
         var label:Label = findComponent(Label);
         if (label != null &&
-            (label.customStyle.color != style.color
-            || label.customStyle.fontName != style.fontName
-            || label.customStyle.fontSize != style.fontSize
-            || label.customStyle.cursor != style.cursor)) {
+            (label.customStyle.color != style.color ||
+            label.customStyle.fontName != style.fontName ||
+            label.customStyle.fontSize != style.fontSize ||
+            label.customStyle.cursor != style.cursor)) {
 
             label.customStyle.color = style.color;
             label.customStyle.fontName = style.fontName;
@@ -97,168 +56,112 @@ class CheckBox extends InteractiveComponent {
             label.invalidateStyle();
         }
     }
-
-    //***********************************************************************************************************
-    // Validation
-    //***********************************************************************************************************
-
-    private override function validateData() {
-        if (behaviourGet("text") != _text) {
-            behaviourSet("text", _text);
-        }
-
-        if (behaviourGet("selected") != _selected) {
-            behaviourSet("selected", _selected);
-            
-            var event:UIEvent = new UIEvent(UIEvent.CHANGE);
-            dispatch(event);
-        }
-    }
-
-    //***********************************************************************************************************
-    // Public API
-    //***********************************************************************************************************
-    private var _selected:Bool = false;
-    /**
-     Whether to show a checkmark in this checkbox component or not
-    **/
-    @:dox(group = "Selection related properties")
-    @:clonable public var selected(get, set):Bool;
-    private function set_selected(value:Bool):Bool {
-        if (value == _selected) {
-            return value;
-        }
-        invalidateData();
-        _selected = value;
-        return value;
-    }
-
-    private function get_selected():Bool {
-        return _selected;
-    }
-
-    private function toggleSelected():Bool {
-        return selected = !selected;
-    }
-
-    //***********************************************************************************************************
-    // Events
-    //***********************************************************************************************************
-    private function _onClick(event:MouseEvent) {
-        toggleSelected();
-        var event:UIEvent = new UIEvent(UIEvent.CHANGE);
-        dispatch(event);
-    }
-
-    private function _onMouseOver(event:MouseEvent) {
-        addClass(":hover");
-        var value:CheckBoxValue = findComponent(CheckBoxValue);
-        if (value != null) {
-            value.addClass(":hover");
-        }
-    }
-
-    private function _onMouseOut(event:MouseEvent) {
-        removeClass(":hover");
-        var value:CheckBoxValue = findComponent(CheckBoxValue);
-        if (value != null) {
-            value.removeClass(":hover");
-        }
-    }
 }
 
 //***********************************************************************************************************
-// Default behaviours
+// Custom children
 //***********************************************************************************************************
-@:dox(hide)
-@:access(haxe.ui.components.CheckBox)
-class CheckBoxDefaultTextBehaviour extends Behaviour {
-    public override function set(value:Variant) {
-        if (value == null || value.isNull) {
-            return;
-        }
-
-        var checkbox:CheckBox = cast _component;
-        var label:Label = checkbox.findComponent(Label);
-        if (label == null) {
-            label = new Label();
-            label.id = "checkbox-label";
-            label.addClass("checkbox-label");
-
-            label.registerEvent(MouseEvent.CLICK, checkbox._onClick);
-            label.registerEvent(MouseEvent.MOUSE_OVER, checkbox._onMouseOver);
-            label.registerEvent(MouseEvent.MOUSE_OUT, checkbox._onMouseOut);
-
-            checkbox.addComponent(label);
-        }
-        label.text = value;
-    }
-
-    public override function get():Variant {
-        var checkbox:CheckBox = cast _component;
-        var label:Label = checkbox.findComponent(Label);
-        if (label == null) {
-            return null;
-        }
-        return label.text;
-    }
-}
-
-@:dox(hide)
-@:access(haxe.ui.components.CheckBox)
-class CheckBoxDefaultSelectedBehaviour extends Behaviour {
-    public override function set(value:Variant) {
-        var checkbox:CheckBox = cast _component;
-        var checkboxValue:CheckBoxValue = checkbox.findComponent(CheckBoxValue);
-        if (checkboxValue == null) {
-            return;
-        }
-
-        if (value == true) {
-            checkboxValue.addClass(":selected");
-        } else {
-            checkboxValue.removeClass(":selected");
-        }
-    }
-
-    public override function get():Variant {
-        var checkbox:CheckBox = cast _component;
-        var checkboxValue:CheckBoxValue = checkbox.findComponent(CheckBoxValue);
-        if (checkboxValue == null) {
-            return false;
-        }
-        return checkboxValue.hasClass(":selected");
-    }
-}
-
-//***********************************************************************************************************
-// Special children
-//***********************************************************************************************************
-/**
- Specialised `InteractiveComponent` used to contain the `CheckBox` icon and respond to style changes
-**/
-@:dox(hide)
-class CheckBoxValue extends InteractiveComponent {
+@:dox(hide) @:noCompletion
+private class Value extends InteractiveComponent {
     public function new() {
         super();
         #if (openfl && !flixel)
         mouseChildren = false;
         #end
+    }
 
-        var icon:Image = findComponent(Image); // its going to be null, but just in case it moves
+    private override function onReady() { // use onReady so we have a parentComponent
+        var icon:Image = findComponent(Image);
         if (icon == null) {
             icon = new Image();
-            icon.id = "checkbox-icon";
-            icon.addClass("checkbox-icon");
+            icon.id = '${parentComponent.cssName}-icon';
+            icon.addClass('${parentComponent.cssName}-icon');
             addComponent(icon);
         }
     }
-
+    
     private override function applyStyle(style:Style) {
         super.applyStyle(style);
         var icon:Image = findComponent(Image);
         if (icon != null) {
             icon.resource = style.icon;
         }
+    }
+}
+
+//***********************************************************************************************************
+// Behaviours
+//***********************************************************************************************************
+@:dox(hide) @:noCompletion
+private class TextBehaviour extends DataBehaviour {
+    private override function validateData() {
+        var label:Label = _component.findComponent(Label, false);
+        if (label == null) {
+            label = new Label();
+            label.id = '${_component.cssName}-label';
+            label.addClass('${_component.cssName}-label');
+            label.scriptAccess = false;
+            _component.addComponent(label);
+        }
+        
+        label.text = _value;
+    }
+}
+
+@:dox(hide) @:noCompletion
+private class SelectedBehaviour extends DataBehaviour {
+    private override function validateData() {
+        var valueComponent:Value = _component.findComponent(Value);
+        if (_value == true) {
+            valueComponent.addClass(":selected");
+        } else {
+            valueComponent.removeClass(":selected");
+        }
+        
+        _component.dispatch(new UIEvent(UIEvent.CHANGE));
+    }
+}
+
+//***********************************************************************************************************
+// Events
+//***********************************************************************************************************
+@:dox(hide) @:noCompletion
+private class Events extends haxe.ui.core.Events  {
+    private var _checkbox:CheckBox;
+    
+    public function new(checkbox:CheckBox) {
+        super(checkbox);
+        _checkbox = checkbox;
+    }
+    public override function register() {
+        if (hasEvent(MouseEvent.MOUSE_OVER, onMouseOver) == false) {
+            registerEvent(MouseEvent.MOUSE_OVER, onMouseOver);
+        }
+        if (hasEvent(MouseEvent.MOUSE_OUT, onMouseOut) == false) {
+            registerEvent(MouseEvent.MOUSE_OUT, onMouseOut);
+        }
+        if (hasEvent(MouseEvent.CLICK, onClick) == false) {
+            registerEvent(MouseEvent.CLICK, onClick);
+        }
+    }
+    
+    public override function unregister() {
+        unregisterEvent(MouseEvent.MOUSE_OVER, onMouseOver);
+        unregisterEvent(MouseEvent.MOUSE_OUT, onMouseOut);
+        unregisterEvent(MouseEvent.CLICK, onClick);
+    }
+    
+    private function onMouseOver(event:MouseEvent) {
+        _target.addClass(":hover");
+        _target.findComponent(Value).addClass(":hover");
+    }
+    
+    private function onMouseOut(event:MouseEvent) {
+        _target.removeClass(":hover");
+        _target.findComponent(Value).removeClass(":hover");
+    }
+    
+    private function onClick(event:MouseEvent) {
+        _checkbox.selected = !_checkbox.selected;
     }
 }
