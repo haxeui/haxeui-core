2c52459104dbe0aaa229bbe8f38a7089f42d936a
Ian Harrigan <ianharrigan@hotmail.com>
2018-10-01 21:04:58 +0200

    move cli files

--- cli/src/runs/android/AndroidRun.hx
+++ cli/src/runs/android/AndroidRun.hx
@@ -6,7 +6,7 @@ class AndroidRun extends Run {
     public override function execute(params:Params) {
         var ANDROID_HOME = Sys.getEnv("ANDROID_HOME");
         
-        var r1 = new ProcessRun(['${ANDROID_HOME}/platform-tools/adb', '-d', 'install', '-r', 'build/android/app/build/outputs/apk/debug/app-debug.apk']);
+        var r1 = new ProcessRun(['${ANDROID_HOME}/platform-tools/adb', 'install', '-r', 'build/android/app/build/outputs/apk/debug/app-debug.apk']);
 //        var r1 = new ProcessRun(['${ANDROID_HOME}/platform-tools/adb', '-d', 'install', '-r', 'build/android/app/build/outputs/apk/release/app-release-unsigned.apk']);
         r1.execute(params);
         
--- cli/templates/android-flash-develop/run-android.bat
+++
@@ -1,2 +0,0 @@
-call haxelib run haxeui-core run android
-call %ANDROID_HOME%/platform-tools/adb -d logcat System.out:I *:S
