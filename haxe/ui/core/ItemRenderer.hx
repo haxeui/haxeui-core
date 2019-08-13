df8bd7b85a1c6fd4a977642f4d008bf660e049b9
Ian Harrigan <ianharrigan@hotmail.com>
2019-08-13 19:55:23 +0200

    Fix for https://github.com/haxeui/haxeui-core/issues/295

--- haxe/ui/core/ItemRenderer.hx
+++ haxe/ui/core/ItemRenderer.hx
@@ -91,25 +91,23 @@ class ItemRenderer extends Box {
             } else {
                 var c:Component = findComponent(f, null, true);
                 if (c != null && v != null) {
-                    if (c != null && v != null) {
-                        var propValue:Dynamic = v;
-                        
-                        if (propValue == "true" || propValue == "yes" || propValue == "false" || propValue == "no") {
-                            propValue = (propValue == "true" || propValue == "yes");
-                        } else if (~/^[0-9]*$/i.match(propValue)) {
-                            propValue = Std.parseInt(propValue);
-                        }
-                        
-                        c.value = propValue;
-                        
-                        if (c.hasEvent(UIEvent.CHANGE, onItemChange) == false) {
-                            c.registerEvent(UIEvent.CHANGE, onItemChange);
-                        }
-                        
-                        c.show();
-                    } else if (c != null) {
-                        c.hide();
+                    var propValue:Dynamic = v;
+                    
+                    if (propValue == "true" || propValue == "yes" || propValue == "false" || propValue == "no") {
+                        propValue = (propValue == "true" || propValue == "yes");
+                    } else if (~/^[0-9]*$/i.match(propValue)) {
+                        propValue = Std.parseInt(propValue);
                     }
+                    
+                    c.value = propValue;
+                    
+                    if (c.hasEvent(UIEvent.CHANGE, onItemChange) == false) {
+                        c.registerEvent(UIEvent.CHANGE, onItemChange);
+                    }
+                    
+                    c.show();
+                } else if (c != null) {
+                    c.hide();
                 }
             }
         }
