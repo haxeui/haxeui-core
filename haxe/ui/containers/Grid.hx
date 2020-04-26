86a38a7b8d9c3e246168b3d0282ab87a328550d9
Ian Harrigan <ianharrigan@hotmail.com>
2020-04-26 12:19:59 +0200

    calendar fixes

--- haxe/ui/containers/Grid.hx
+++ haxe/ui/containers/Grid.hx
@@ -8,16 +8,33 @@ import haxe.ui.layouts.VerticalGridLayout;
 class Grid extends Box {
     public function new() {
         super();
-        layout = new VerticalGridLayout();
-        cast(_layout, VerticalGridLayout).columns = 2;
+        if (_columns == -1) { // dont set it if its already been set
+            columns = 2;
+        }
     }
     
+    //***********************************************************************************************************
+    // Public API
+    //***********************************************************************************************************
+    private var _columns:Int = -1;
     @:clonable public var columns(get, set):Int;
     private function get_columns():Int {
         return cast(_layout, VerticalGridLayout).columns;
     }
     private function set_columns(value:Int):Int {
+        if (_layout == null) {
+            layout = createLayout();
+        }
         cast(_layout, VerticalGridLayout).columns = value;
+        _columns = value;
         return value;
+    }
+    
+    //***********************************************************************************************************
+    // Internals
+    //***********************************************************************************************************
+    private override function createDefaults() {
+        super.createDefaults();
+        _defaultLayoutClass = VerticalGridLayout;
     }
 }
