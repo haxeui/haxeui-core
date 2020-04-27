f26b36346cb918d5f0edc954ea4dc5bae88cafbe
Ian Harrigan <ianharrigan@hotmail.com>
2020-04-27 18:57:19 +0200

    tab tweak

--- haxe/ui/containers/TabView.hx
+++ haxe/ui/containers/TabView.hx
@@ -46,15 +46,15 @@ private class Layout extends DefaultLayout {
             
             tabs.left = paddingLeft;
             if (tabs.height != 0) {
-                tabs.top = component.height - tabs.height - paddingBottom;
+                tabs.top = component.height - tabs.height - paddingBottom + marginTop(tabs);
             }
         } else {
             tabs.left = paddingLeft;
-            tabs.top = paddingTop;
+            tabs.top = paddingTop + marginTop(tabs);
 
             content.left = paddingLeft;
             if (tabs.height != 0) {
-                content.top = tabs.top + tabs.height - 1;
+                content.top = tabs.top + tabs.height - marginTop(tabs);
             }
         }
     }
