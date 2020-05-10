764d2ea6e065c0b04748b21f6b01fefa907c4038
Ian Harrigan <ianharrigan@hotmail.com>
2020-05-10 13:23:21 +0200

    propertygrid changes

--- haxe/ui/containers/properties/PropertyGrid.hx
+++ haxe/ui/containers/properties/PropertyGrid.hx
@@ -1,11 +1,13 @@
 package haxe.ui.containers.properties; 
 
+import haxe.ui.behaviours.DefaultBehaviour;
 import haxe.ui.containers.ScrollView.ScrollViewBuilder;
 import haxe.ui.containers.ScrollView.ScrollViewEvents;
 import haxe.ui.core.CompositeBuilder;
 
 @:composite(Events, Builder)
 class PropertyGrid extends ScrollView {
+    @:behaviour(DefaultBehaviour)                    public var popupStyleNames:String;
 }
 
 //***********************************************************************************************************
