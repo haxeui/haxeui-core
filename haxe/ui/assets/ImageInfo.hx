7631ed70e3c680805b79bb688158e4ff21eceb25
Ian Harrigan <ianharrigan@hotmail.com>
2020-09-16 09:10:46 +0200

    Fix for https://github.com/haxeui/haxeui-openfl/issues/51

--- haxe/ui/assets/ImageInfo.hx
+++ haxe/ui/assets/ImageInfo.hx
@@ -3,7 +3,13 @@ package haxe.ui.assets;
 import haxe.ui.backend.ImageData;
 
 typedef ImageInfo = {
+    #if svg
+    @:optional public var data:ImageData;
+    @:optional public var svg:format.SVG;
+    #else
     public var data:ImageData;
+    #end
     public var width:Int;
     public var height:Int;
+    
 }
