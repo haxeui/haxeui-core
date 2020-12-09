e830d3c6d7ac8104a5b5ab0ef6c42b20c4a9960a
David <intoxopox@gmail.com>
2020-12-09 12:39:46 -0500

    Drag-drop support w/ DragManager & DragOptions
    Usage ex: DragManager.instance.registerDraggable(myComp, {mouseTarget: subComp, dragTolerance: 3});

---
+++ haxe/ui/dragdrop/DragOptions.hx
@@ -0,0 +1,36 @@
+package haxe.ui.dragdrop;
+
+import haxe.ui.geom.Rectangle;
+import haxe.ui.core.Component;
+
+typedef DragOptions = {
+    /**
+     * A component that will trigger dragging - usually the draggable component or a sub-component of it.
+     * Default: the draggable component
+     */
+    var ?mouseTarget:Component;
+
+    /**
+     * The x offset to be added to the moveTarget during drag in addition to the mouse x offset from the moveTarget's x..
+     * Default: 0
+     */
+    var ?dragOffsetX:Float;
+
+    /**
+     * The y offset to be added to the moveTarget during drag in addition to the mouse y offset from the moveTarget's y.
+     * Default: 0
+     */
+    var ?dragOffsetY:Float;
+
+    /**
+     * The distance the mouse must travel while mouseDown on the mouseTarget before drag begins.
+     * Default: 1
+     */
+    var ?dragTolerance:Int;
+
+    /**
+     * A Rect specifying the bounds in the screen's coordinate space.
+     * Default: screen's bounds
+     */
+    var ?dragBounds:Rectangle;
+}
