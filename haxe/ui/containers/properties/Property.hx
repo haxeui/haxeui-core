cf749c1cb1a3734ff3b54c92fad6b2222e18ae7f
Ian Harrigan <ianharrigan@hotmail.com>
2020-12-11 17:02:17 +0100

    add getter for property value

--- haxe/ui/containers/properties/Property.hx
+++ haxe/ui/containers/properties/Property.hx
@@ -84,6 +84,14 @@ private class PropertyValueBehaviour extends ComponentValueBehaviour {
             builder.editor.value = Variant.toDynamic(value);
         }
     }
+    
+    public override function get():Variant {
+        var builder = cast(_property._compositeBuilder, PropertyBuilder);
+        if (builder.editor != null) {
+            return builder.editor.value;
+        }
+        return null;
+    }
 }
 
 //***********************************************************************************************************
