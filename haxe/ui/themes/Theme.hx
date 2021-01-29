7b2ccbe4df3d742f6914d1daec1ba31aa332f535
Ian Harrigan <ianharrigan@hotmail.com>
2021-01-29 15:13:52 +0100

    theme names

--- haxe/ui/themes/Theme.hx
+++ haxe/ui/themes/Theme.hx
@@ -1,6 +1,9 @@
 package haxe.ui.themes;
 
 class Theme {
+    public static inline var DEFAULT:String = "default";
+    public static inline var DARK:String = "dark";
+    
     public var parent:String;
     public var styles:Array<ThemeEntry>;
     public var images:Array<ThemeImageEntry>;
