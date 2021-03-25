011ac5bed295ae42d4255601bab3b8b3977d1b58
Ian Harrigan <ianharrigan@hotmail.com>
2021-03-25 21:07:53 +0100

    ability to reset groups

--- haxe/ui/components/OptionBox.hx
+++ haxe/ui/components/OptionBox.hx
@@ -1,10 +1,11 @@
 package haxe.ui.components;
 
 import haxe.ds.StringMap;
+import haxe.ui.behaviours.Behaviour;
+import haxe.ui.behaviours.DataBehaviour;
 import haxe.ui.components.CheckBox.CheckBoxBuilder;
 import haxe.ui.components.CheckBox.CheckBoxValue;
 import haxe.ui.core.Component;
-import haxe.ui.behaviours.DataBehaviour;
 import haxe.ui.events.UIEvent;
 import haxe.ui.util.Variant;
 
@@ -13,10 +14,11 @@ class OptionBox extends CheckBox {
     //***********************************************************************************************************
     // Public API
     //***********************************************************************************************************
-    @:clonable @:behaviour(GroupBehaviour, "defaultGroup")     public var componentGroup:String;
-    @:clonable @:behaviour(SelectedBehaviour)                  public var selected:Bool;
-    @:clonable @:behaviour(SelectedOptionBehaviour)            public var selectedOption:Component;
-    @:clonable @:value(selected)                               public var value:Any;
+    @:clonable @:behaviour(GroupBehaviour, "defaultGroup")      public var componentGroup:String;
+    @:clonable @:behaviour(SelectedBehaviour)                   public var selected:Bool;
+    @:clonable @:behaviour(SelectedOptionBehaviour)             public var selectedOption:Component;
+    @:clonable @:value(selected)                                public var value:Any;
+    @:call(ResetGroup)                                          public function resetGroup():Void;
 }
 
 //***********************************************************************************************************
@@ -34,47 +36,7 @@ private class GroupBehaviour extends DataBehaviour {
 private class SelectedBehaviour extends DataBehaviour {
     public override function validateData() {
         var optionbox:OptionBox = cast(_component, OptionBox);
-
-        if (optionbox.componentGroup != null && _value == false) { // dont allow false if no other group selection
-            var arr:Array<OptionBox> = OptionBoxGroups.instance.get(optionbox.componentGroup);
-            var hasSelection:Bool = false;
-            if (arr != null) {
-                for (option in arr) {
-                    if (option != _component && option.selected == true) {
-                        hasSelection = true;
-                        break;
-                    }
-                }
-            }
-            if (hasSelection == false) {
-                _value = true;
-                return;
-            }
-        }
-
-        if (optionbox.componentGroup != null && _value == true) { // set all the others in group
-            var arr:Array<OptionBox> = OptionBoxGroups.instance.get(optionbox.componentGroup);
-            if (arr != null) {
-                for (option in arr) {
-                    if (option != _component) {
-                        option.selected = false;
-                    }
-                }
-            }
-        }
-
-        var valueComponent:CheckBoxValue = _component.findComponent("optionbox-value", CheckBoxValue);
-        if (valueComponent == null) {
-            return;
-        }
-        valueComponent.createIcon();
-        if (_value == true) {
-            valueComponent.addClass(":selected");
-            _component.dispatch(new UIEvent(UIEvent.CHANGE));
-        } else {
-            valueComponent.removeClass(":selected");
-        }
-
+        cast(_component._compositeBuilder, OptionBoxBuilder).setSelection(optionbox, _value);
     }
 }
 
@@ -96,13 +58,69 @@ private class SelectedOptionBehaviour extends DataBehaviour {
     }
 }
 
+@:dox(hide) @:noCompletion
+@:access(haxe.ui.core.Component)
+private class ResetGroup extends Behaviour {
+    public override function call(param:Any = null):Variant {
+        var optionbox:OptionBox = cast(_component, OptionBox);
+        OptionBoxGroups.instance.reset(optionbox.componentGroup);
+        return null;
+    }
+}
 //***********************************************************************************************************
 // Composite Builder
 //***********************************************************************************************************
 @:dox(hide) @:noCompletion
+@:access(haxe.ui.core.Component)
 class OptionBoxBuilder extends CheckBoxBuilder {
     private override function get_cssName():String {
         return "optionbox";
+    }
+    
+    public function setSelection(optionbox:OptionBox, value:Bool, allowDeselection:Bool = false) {
+        if (optionbox.componentGroup != null && value == false) { // dont allow false if no other group selection
+            var arr:Array<OptionBox> = OptionBoxGroups.instance.get(optionbox.componentGroup);
+            var hasSelection:Bool = false;
+            if (arr != null) {
+                for (option in arr) {
+                    if (option != optionbox && option.selected == true) {
+                        hasSelection = true;
+                        break;
+                    }
+                }
+            }
+            if (hasSelection == false && allowDeselection == false) {
+                value = true;
+                return;
+            }
+        }
+
+        if (optionbox.componentGroup != null && value == true) { // set all the others in group
+            var arr:Array<OptionBox> = OptionBoxGroups.instance.get(optionbox.componentGroup);
+            if (arr != null) {
+                for (option in arr) {
+                    if (option != optionbox) {
+                        option.selected = false;
+                    }
+                }
+            }
+        }
+
+        if (allowDeselection == true && value == false) {
+            @:privateAccess optionbox.behaviours.find("selected").set(false);
+        }
+        
+        var valueComponent:CheckBoxValue = optionbox.findComponent("optionbox-value", CheckBoxValue);
+        if (valueComponent == null) {
+            return;
+        }
+        valueComponent.createIcon();
+        if (value == true) {
+            valueComponent.addClass(":selected");
+            optionbox.dispatch(new UIEvent(UIEvent.CHANGE));
+        } else {
+            valueComponent.removeClass(":selected");
+        }
     }
 }
 
@@ -110,6 +128,7 @@ class OptionBoxBuilder extends CheckBoxBuilder {
 // Util classes
 //***********************************************************************************************************
 @:dox(hide) @:noCompletion
+@:access(haxe.ui.core.Component)
 class OptionBoxGroups { // singleton
     private static var _instance:OptionBoxGroups;
     public static var instance(get, null):OptionBoxGroups;
@@ -146,5 +165,26 @@ class OptionBoxGroups { // singleton
             arr.push(optionbox);
         }
         set(name, arr);
+    }
+    
+    public function reset(name:String) {
+        var arr:Array<OptionBox> = get(name);
+        if (arr == null) {
+            return;
+        }
+        
+        var selection = null;
+        for (item in arr) {
+            if (item.selected == true) {
+                selection = item;
+                break;
+            }
+        }
+        
+        if (selection == null) {
+            return;
+        }
+        
+        cast(selection._compositeBuilder, OptionBoxBuilder).setSelection(selection, false, true);
     }
 }
