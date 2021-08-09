912a7f7ff6a8a5927f709097b8d355eed5c90d90
Ian Harrigan <ianharrigan@hotmail.com>
2021-08-09 12:18:04 +0200

    CLI to use new ComponentBuilder

--- cli/templates/shared/MainView.hx
+++ cli/templates/shared/MainView.hx
@@ -3,7 +3,7 @@ package ::package::;
 import haxe.ui.containers.VBox;
 import haxe.ui.events.MouseEvent;
 
-@:build(haxe.ui.macros.ComponentMacros.build("assets/main-view.xml"))
+@:build(haxe.ui.ComponentBuilder.build("assets/main-view.xml"))
 class MainView extends VBox {
     public function new() {
         super();
