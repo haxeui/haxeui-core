be99032148c53d25c7abc809001cde2f766f88a1
Ian Harrigan <ianharrigan@hotmail.com>
2022-01-25 10:59:40 +0100

    syncComponentValidation

--- haxe/ui/components/DropDown.hx
+++ haxe/ui/components/DropDown.hx
@@ -630,6 +630,7 @@ class DropDownEvents extends ButtonEvents {
             _wrapper.top = _dropdown.screenTop + (_dropdown.actualComponentHeight - Toolkit.scaleY) + componentOffset.y;
             Screen.instance.addComponent(_wrapper);
             handler.prepare(_wrapper);
+            _wrapper.syncComponentValidation();
             _wrapper.validateNow();
 
             var cx = _wrapper.width - _dropdown.width;
