710dcee6e19907aae957a4f82c4d6dcdac6140ab
Ian Harrigan <ianharrigan@hotmail.com>
2022-02-23 11:25:40 +0100

    Platform::isMobile

--- haxe/ui/backend/PlatformBase.hx
+++ haxe/ui/backend/PlatformBase.hx
@@ -43,6 +43,34 @@ class PlatformBase {
         return false;
     }
     
+    #if js
+    private static var MOBILE_REGEXP = new EReg("(android|bb\\d+|meego).+mobile|avantgo|bada\\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\\.(browser|link)|vodafone|wap|windows ce|xda|xiino", "gi");
+    #end
+    
+    private var _isMobile:Null<Bool> = null;
+    public var isMobile(get, null):Bool;
+    private function get_isMobile():Bool {
+        #if mobile
+        return true;
+        #end
+        
+        if (_isMobile != null) {
+            return _isMobile;
+        }
+        
+        _isMobile = false;
+        
+        #if js
+        
+        var ua = js.Browser.navigator.userAgent;
+        _isMobile = MOBILE_REGEXP.match(ua);
+        
+        #end
+        
+        return _isMobile;
+    }
+    
+    
     public function getMetric(id:String):Float {
         return 0;
     }
