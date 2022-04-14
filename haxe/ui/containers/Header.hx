a2ebcc0deab60c0b6bb447c9f0b7ac72e45da964
Ian Harrigan <ianharrigan@hotmail.com>
2022-04-14 23:34:20 +0200

    fix for https://github.com/haxeui/haxeui-core/issues/442

--- haxe/ui/containers/Header.hx
+++ haxe/ui/containers/Header.hx
@@ -74,7 +74,7 @@ private class Layout extends HorizontalLayout {
                 continue;
             }
             
-            if (child.text == null || child.text.length == 0) {
+            if (child.text == null || child.text.length == 0 || child.height < max) {
                 child.height = max;
             }
         }
