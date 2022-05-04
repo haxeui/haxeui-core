7bd3f48c641805c15c7d7b09793bcf60494b4f20
Ian Harrigan <ianharrigan@hotmail.com>
2022-05-04 11:26:50 +0200

    allow for non-equal sized buttons

--- haxe/ui/containers/HorizontalButtonBar.hx
+++ haxe/ui/containers/HorizontalButtonBar.hx
@@ -2,9 +2,39 @@ package haxe.ui.containers;
 
 import haxe.ui.layouts.HorizontalLayout;
 
-@:composite(HorizontalLayout)
+@:composite(Layout)
 class HorizontalButtonBar extends ButtonBar {
     public function new() {
         super();
+    }
+}
+
+//***********************************************************************************************************
+// Composite Layout
+//***********************************************************************************************************
+private class Layout extends HorizontalLayout {
+    private override function resizeChildren() {
+        super.resizeChildren();
+
+        var max:Float = 0;
+        for (child in component.childComponents) {
+            if (child.includeInLayout == false) {
+                continue;
+            }
+            
+            if (child.height > max) {
+                max = child.height;
+            }
+        }
+        
+        for (child in component.childComponents) {
+            if (child.includeInLayout == false) {
+                continue;
+            }
+            
+            if (child.text == null || child.text.length == 0 || child.height < max) {
+                child.height = max;
+            }
+        }
     }
 }
--- haxe/ui/containers/VerticalButtonBar.hx
+++ haxe/ui/containers/VerticalButtonBar.hx
@@ -2,9 +2,39 @@ package haxe.ui.containers;
 
 import haxe.ui.layouts.VerticalLayout;
 
-@:composite(VerticalLayout)
+@:composite(Layout)
 class VerticalButtonBar extends ButtonBar {
     public function new() {
         super();
+    }
+}
+
+//***********************************************************************************************************
+// Composite Layout
+//***********************************************************************************************************
+private class Layout extends VerticalLayout {
+    private override function resizeChildren() {
+        super.resizeChildren();
+
+        var max:Float = 0;
+        for (child in component.childComponents) {
+            if (child.includeInLayout == false) {
+                continue;
+            }
+            
+            if (child.width > max) {
+                max = child.width;
+            }
+        }
+        
+        for (child in component.childComponents) {
+            if (child.includeInLayout == false) {
+                continue;
+            }
+            
+            if (child.text == null || child.text.length == 0 || child.width < max) {
+                child.width = max;
+            }
+        }
     }
 }
