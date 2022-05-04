4dad8cd546e6018bbcb55fdc0a155186855d06c4
Ian Harrigan <ianharrigan@hotmail.com>
2022-05-04 09:31:30 +0200

    take horizontal and vertical variants into account for rtti

--- haxe/ui/core/TypeMap.hx
+++ haxe/ui/core/TypeMap.hx
@@ -2,10 +2,19 @@ package haxe.ui.core;
 
 import haxe.ui.util.RTTI;
 
+using StringTools;
+
 class TypeMap {
     public static function getTypeInfo(className:String, property:String):String {
         var entry = RTTI.getClassInfo(className);
         if (entry == null) {
+            var parts = className.split(".");
+            var name = parts.pop();
+            if (name.startsWith("Horizontal")) {
+                return getTypeInfo(parts.join(".") + "." + name.substr("Horizontal".length), property);
+            } else if (name.startsWith("Vertical")) {
+                return getTypeInfo(parts.join(".") + "." + name.substr("Vertical".length), property);
+            }
             return null;
         }
         
