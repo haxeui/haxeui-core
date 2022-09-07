f42de4da9dcd5cc819a9b886397559ea935c9aad
Ian Harrigan <ianharrigan@hotmail.com>
2022-09-07 09:56:30 +0200

    issue warning creating base class

--- haxe/ui/components/Rule.hx
+++ haxe/ui/components/Rule.hx
@@ -1,11 +1,13 @@
 package haxe.ui.components;
 
 import haxe.ui.core.Component;
+import haxe.ui.core.CompositeBuilder;
 import haxe.ui.core.IDirectionalComponent;
 
 /**
  * A simple rule component, that crosses the parent container.
  */
+@:composite(RuleBuilder)
 class Rule extends Component implements IDirectionalComponent {
 
     /**
@@ -16,5 +18,20 @@ class Rule extends Component implements IDirectionalComponent {
         #if (haxeui_openfl && !haxeui_flixel)
         mouseChildren = false;
         #end
+    }
+}
+
+//***********************************************************************************************************
+// Composite Builder
+//***********************************************************************************************************
+@:dox(hide) @:noCompletion
+class RuleBuilder extends CompositeBuilder {
+    public function new(component:Component) {
+        super(component);
+        showWarning();
+    }
+    
+    private function showWarning() {
+        trace("WARNING: trying to create an instance of 'Rule' directly, use either 'HorizontalRule' or 'VerticalRule'");
     }
 }
