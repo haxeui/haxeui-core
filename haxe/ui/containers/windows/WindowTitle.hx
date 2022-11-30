4a65f80b34547596b641a0ba18afbbb16a56c71f
Ian Harrigan <ianharrigan@hotmail.com>
2022-12-01 00:36:26 +0100

    quote needed for haxe 3

--- haxe/ui/containers/windows/WindowTitle.hx
+++ haxe/ui/containers/windows/WindowTitle.hx
@@ -2,12 +2,12 @@ package haxe.ui.containers.windows;
 
 import haxe.ui.core.Component;
 
-@:xml(
+@:xml('
     <hbox width="100%">
         <label id="windowTitleLabel" text="Window" />
         <hbox id="controlsContainer" hidden="true" verticalAlign="center" style="cursor: default" />
     </hbox>
-)
+')
 class WindowTitle extends HBox {
     public override function addComponent(child:Component):Component {
         if (controlsContainer != null && child != windowTitleLabel && child != controlsContainer) {
