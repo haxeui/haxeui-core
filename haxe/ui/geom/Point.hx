c7e0ada30a75bb21048788c6431bd71e303145e0
Gulvan <mitmitmitmit@yandex.ru>
2023-01-05 14:41:51 +0300

    Update Point.hx

--- haxe/ui/geom/Point.hx
+++ haxe/ui/geom/Point.hx
@@ -13,6 +13,14 @@ class Point {
         this.x -= subtrahend.x;
         this.y -= subtrahend.y;
     }
+    
+    public function sum(addend:Point):Point {
+        return new Point(this.x + addend.x, this.y + addend.y);
+    }
+    
+    public function diff(subtrahend:Point):Point {
+        return new Point(this.x - subtrahend.x, this.y - subtrahend.y);
+    }
 
     public function new(x:Float = 0, y:Float = 0) {
         this.x = x;
