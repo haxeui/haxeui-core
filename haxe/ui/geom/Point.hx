a84cd2ae22d18b86e52a5d3130666c28585ee509
Gulvan <mitmitmitmit@yandex.ru>
2023-01-05 14:38:47 +0300

    Elementary point arithmetics

--- haxe/ui/geom/Point.hx
+++ haxe/ui/geom/Point.hx
@@ -3,6 +3,16 @@ package haxe.ui.geom;
 class Point {
     public var x:Float;
     public var y:Float;
+    
+    public function add(addend:Point) {
+        this.x += addend.x;
+        this.y += addend.y;
+    }
+    
+    public function subtract(subtrahend:Point) {
+        this.x -= subtrahend.x;
+        this.y -= subtrahend.y;
+    }
 
     public function new(x:Float = 0, y:Float = 0) {
         this.x = x;
