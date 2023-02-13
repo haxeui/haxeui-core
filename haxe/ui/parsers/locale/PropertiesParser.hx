67aa20ed061c609dd4c7e2416fd1b133ced32398
Ian Harrigan <ianharrigan@hotmail.com>
2023-02-13 15:03:31 +0100

    dont add trimmed string to newLines (keep spaces)

--- haxe/ui/parsers/locale/PropertiesParser.hx
+++ haxe/ui/parsers/locale/PropertiesParser.hx
@@ -10,8 +10,7 @@ class PropertiesParser extends LocaleParser {
         // bit cheeky, but lets strip comments and empty lines
         // before we start parsing
         for (line in lines) {
-            line = line.trim();
-            if (line.length == 0 || line.startsWith("#")) {
+            if (line.trim().length == 0 || line.startsWith("#")) {
                 continue;
             }
 
