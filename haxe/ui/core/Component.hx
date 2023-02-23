5545c03718553e26ff62b90200b5821ee81de38e
Ian Harrigan <ianharrigan@hotmail.com>
2023-02-23 12:38:24 +0100

    Revert "dispatch resize event AFTER component has actually resized"
    This reverts commit 1c4243d68008a8d81b607ddd9bd2f88d597f3da7.

--- haxe/ui/core/Component.hx
+++ haxe/ui/core/Component.hx
@@ -1882,6 +1882,7 @@ class Component extends ComponentImpl implements IValidating {
             }
 
             onResized();
+            dispatch(new UIEvent(UIEvent.RESIZE));
 
             sizeChanged = true;
         }
@@ -1995,8 +1996,6 @@ class Component extends ComponentImpl implements IValidating {
         }
 
         handleSize(componentWidth, componentHeight, _style);
-
-        dispatch(new UIEvent(UIEvent.RESIZE));
 
         if (_componentClipRect != null ||
             (style != null && style.clip != null && style.clip == true)) {
