a1b52de356e3b97e2404d515ef3faa3fcec26863
Ian Harrigan <ianharrigan@hotmail.com>
2023-02-23 17:09:54 +0100

    conditionally dispatch resize AFTER component has resized

--- haxe/ui/core/Component.hx
+++ haxe/ui/core/Component.hx
@@ -1882,7 +1882,9 @@ class Component extends ComponentImpl implements IValidating {
             }
 
             onResized();
+            #if !(haxeui_hxwidgets) // TODO: temp until better way
             dispatch(new UIEvent(UIEvent.RESIZE));
+            #end
 
             sizeChanged = true;
         }
@@ -1996,6 +1998,10 @@ class Component extends ComponentImpl implements IValidating {
         }
 
         handleSize(componentWidth, componentHeight, _style);
+
+        #if (haxeui_hxwidgets) // TODO: temp until better way
+        dispatch(new UIEvent(UIEvent.RESIZE));
+        #end
 
         if (_componentClipRect != null ||
             (style != null && style.clip != null && style.clip == true)) {
