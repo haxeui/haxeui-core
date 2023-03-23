b66e5717c913c4bcd60a7ffed6101ebeb0419034
Ian Harrigan <ianharrigan@hotmail.com>
2023-03-23 11:57:06 +0100

    prefix parser constants with PARSER_ (fixes hxcpp / cpp conflicts)

--- haxe/ui/parsers/locale/KeyValueParser.hx
+++ haxe/ui/parsers/locale/KeyValueParser.hx
@@ -3,29 +3,29 @@ package haxe.ui.parsers.locale;
 using StringTools;
 
 class KeyValueParser extends LocaleParser {
-	var SEPARATOR:String = "";
-	var COMMENT_STRING:String = "";
-	var LINE_FEED:String = "\n";
-	var STRICT:Bool = false; // do not allow parser to continue if a line is incorrect
+	var PARSER_SEPARATOR:String = "";
+	var PARSER_COMMENT_STRING:String = "";
+	var PARSER_LINE_FEED:String = "\n";
+	var PARSER_STRICT:Bool = false; // do not allow parser to continue if a line is incorrect
 	
 	public override function parse(data:String):Map<String, String> {
-		if (SEPARATOR == "") {
+		if (PARSER_SEPARATOR == "") {
 			throw "separator, comment needs implementation";
 		}
 		
 		var result:Map<String, String> = new Map<String, String>();
-		var lines = data.split(LINE_FEED);
+		var lines = data.split(PARSER_LINE_FEED);
 		for (line in lines) {
 			line = line.trim();
-			if (line.length == 0 || (line.startsWith(COMMENT_STRING) && COMMENT_STRING != "")) {
+			if (line.length == 0 || (line.startsWith(PARSER_COMMENT_STRING) && PARSER_COMMENT_STRING != "")) {
 				continue;
 			}
 
-			var separator:Int = line.indexOf(SEPARATOR);
+			var separator:Int = line.indexOf(PARSER_SEPARATOR);
 			if (separator == -1) {
 				// skip the line if separator is not found else throw if strict
-				if (STRICT)
-					throw 'Locale parser: Invalid line ${line}. Missing separator $SEPARATOR';
+				if (PARSER_STRICT)
+					throw 'Locale parser: Invalid line ${line}. Missing separator $PARSER_SEPARATOR';
 				else continue;
 			}
 
