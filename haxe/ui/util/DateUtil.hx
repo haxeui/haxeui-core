3cce3ab8ce9f9b9d6d49db55e2ffcd54487f18bf
Ian Harrigan <ianharrigan@hotmail.com>
2023-05-27 13:53:02 +0200

    should be public

--- haxe/ui/util/DateUtil.hx
+++ haxe/ui/util/DateUtil.hx
@@ -2,8 +2,8 @@ package haxe.ui.util;
 
 class DateUtil {
     public static var DAYS_IN_MONTH:Array<Int> = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    private static var DAY_NAMES:Array<String> = ["{{sunday}}", "{{monday}}", "{{tuesday}}", "{{wednesday}}", "{{thursday}}", "{{friday}}", "{{saturday}}"];
-    private static var SHORT_DAY_NAMES:Array<String> = ["{{sun}}", "{{mon}}", "{{tues}}", "{{wed}}", "{{thur}}", "{{fri}}", "{{sat}}"];
+    public static var DAY_NAMES:Array<String> = ["{{sunday}}", "{{monday}}", "{{tuesday}}", "{{wednesday}}", "{{thursday}}", "{{friday}}", "{{saturday}}"];
+    public static var SHORT_DAY_NAMES:Array<String> = ["{{sun}}", "{{mon}}", "{{tues}}", "{{wed}}", "{{thur}}", "{{fri}}", "{{sat}}"];
     public static var MONTH_NAMES:Array<String> = ["{{january}}", "{{febuary}}", "{{march}}", "{{april}}", "{{may}}", "{{june}}", "{{july}}", "{{august}}", "{{september}}", "{{october}}", "{{november}}", "{{december}}"];
     public static var SHORT_MONTH_NAMES:Array<String> = ["{{jan}}", "{{feb}}", "{{mar}}", "{{apr}}", "{{may}}", "{{jun}}", "{{jul}}", "{{aug}}", "{{sep}}", "{{oct}}", "{{nov}}", "{{dec}}"];
     
