a6b1b2bb2889a5a1b8752d027dba6fea00acebfd
inc0der <inc0der.dev@gmail.com>
2023-08-17 01:49:38 -0300

    add userdata to tabbar button

--- haxe/ui/components/TabBar.hx
+++ haxe/ui/components/TabBar.hx
@@ -447,6 +447,7 @@ private class Builder extends CompositeBuilder {
         button.tooltip = child.tooltip;
         if ((child is Button)) {
             button.icon = cast(child, Button).icon;
+            button.userData = cast(child, Button).userData;
         }
         button.closable = _tabbar.closable;
         if (_tabbar.buttonWidth != null) {
