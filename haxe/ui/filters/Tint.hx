33b9fa05bd1a3530e11dbff6ce6bf606e0c9d4ce
Ian Harrigan <ianharrigan@hotmail.com>
2023-09-26 16:18:24 +0200

    normalise tint params

--- haxe/ui/filters/Tint.hx
+++ haxe/ui/filters/Tint.hx
@@ -4,11 +4,18 @@ class Tint extends Filter {
     public static var DEFAULTS:Array<Any> = [0, 1];
 
     public var color:Int;
-    public var amount:Float;
+    public var amount:Float; // from 0 -> 1
 
     public override function parse(filterDetails:Array<Any>) {
         var copy = Filter.applyDefaults(filterDetails, DEFAULTS);
         this.color = copy[0];
         this.amount = copy[1];
+
+        if (this.amount < 0) {
+            this.amount = 0;
+        } else if (this.amount > 1) {
+            this.amount = 1;
+        }
+        
     }
 }
