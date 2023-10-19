a9cb995bf14c7609f9fc81074ca3d76c94e0d392
Ian Harrigan <ianharrigan@hotmail.com>
2023-10-19 09:56:02 +0200

    dont dispatch events on invalid targets

--- haxe/ui/events/Events.hx
+++ haxe/ui/events/Events.hx
@@ -3,6 +3,7 @@ package haxe.ui.events;
 import haxe.ui.actions.ActionType;
 import haxe.ui.core.Component;
 
+@:access(haxe.ui.core.Component)
 class Events {
     private var _target:Component;
 
@@ -19,20 +20,33 @@ class Events {
     }
 
     private function registerEvent<T:UIEvent>(type:EventType<T>, listener:T->Void, priority:Int = 0) {
+        if (_target == null || _target._destroyed) {
+            return;
+        }
         if (hasEvent(type, listener) == false) {
             _target.registerEvent(type, listener, priority);
         }
     }
 
     private function hasEvent<T:UIEvent>(type:EventType<T>, listener:T->Void):Bool {
+        if (_target == null || _target._destroyed) {
+            return false;
+        }
         return _target.hasEvent(type, listener);
     }
 
     private function unregisterEvent<T:UIEvent>(type:EventType<T>, listener:T->Void) {
+        if (_target == null || _target._destroyed) {
+            return;
+        }
         _target.unregisterEvent(type, listener);
     }
 
     private function dispatch<T:UIEvent>(event:T) {
+        if (_target == null || _target._destroyed) {
+            return;
+        }
+        trace(@:privateAccess _target._destroyed);
         _target.dispatch(event);
     }
 }
