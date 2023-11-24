aec0b0fb4efb3eded1c631df9052bea111504fb4
Ian Harrigan <ianharrigan@hotmail.com>
2023-11-24 14:09:44 +0100

    capitilize button for consistency

--- haxe/ui/containers/TabView.hx
+++ haxe/ui/containers/TabView.hx
@@ -417,7 +417,7 @@ private class Builder extends CompositeBuilder {
             button.icon = icon;
             button.tooltip = child.tooltip;
             if (child.id != null) {
-                button.id = child.id + "_button";
+                button.id = child.id + "Button";
             }
             if (child.styleNames != null) {
                 button.styleNames = child.styleNames;
