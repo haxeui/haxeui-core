3c4f03c96602658867c048f7398e31403a210740
Ian Harrigan <ianharrigan@hotmail.com>
2024-02-04 11:45:11 +0100

    dont resize canvas if it doesnt have dimensions

--- haxe/ui/components/Canvas.hx
+++ haxe/ui/components/Canvas.hx
@@ -19,6 +19,12 @@ class Canvas extends Component implements IDataComponent {
     
     private override function validateComponentLayout():Bool {
         var b = super.validateComponentLayout();
+        if (width == null || height == null) {
+            return b;
+        }
+        if (width <= 0  || height <= 0) {
+            return b;
+        }
         componentGraphics.resize(width, height);
         return b;
     }
