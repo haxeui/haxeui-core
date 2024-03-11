31e25363b49984fc31f1631a79786b3c4e0b256b
Ian Harrigan <ianharrigan@hotmail.com>
2024-03-11 22:03:19 +0100

    allow boxes to use data sources (take 2)

--- haxe/ui/containers/Box.hx
+++ haxe/ui/containers/Box.hx
@@ -3,6 +3,12 @@ package haxe.ui.containers;
 import haxe.ui.behaviours.DataBehaviour;
 import haxe.ui.behaviours.DefaultBehaviour;
 import haxe.ui.core.Component;
+import haxe.ui.core.CompositeBuilder;
+import haxe.ui.core.IDataComponent;
+import haxe.ui.core.ItemRenderer;
+import haxe.ui.data.ArrayDataSource;
+import haxe.ui.data.DataSource;
+import haxe.ui.events.UIEvent;
 import haxe.ui.layouts.DefaultLayout;
 import haxe.ui.layouts.LayoutFactory;
 import haxe.ui.styles.Style;
@@ -13,7 +19,7 @@ import haxe.ui.util.Variant;
 **/
 @:dox(icon = "/icons/ui-panel.png")
 @:composite(DefaultLayout)
-class Box extends Component {
+class Box extends Component implements IDataComponent {
     //***********************************************************************************************************
     // Public API
     //***********************************************************************************************************
@@ -24,6 +30,7 @@ class Box extends Component {
      of it should they want to
     **/
     @:clonable @:behaviour(DefaultBehaviour)                public var icon:Variant;
+    @:clonable @:behaviour(DataSourceBehaviour)             public var dataSource:DataSource<Dynamic>;
 
     @:noCompletion private var _layoutName:String;
     @:clonable public var layoutName(get, set):String;
@@ -40,6 +47,20 @@ class Box extends Component {
         if (l != null) {
             layout = l;
         }
+        return value;
+    }
+
+    private var _itemRenderer:ItemRenderer;
+    @:clonable public var itemRenderer(get, set):ItemRenderer;
+    private function get_itemRenderer():ItemRenderer {
+        return _itemRenderer;
+    }
+    private function set_itemRenderer(value:ItemRenderer):ItemRenderer {
+        if (_itemRenderer != value) {
+            _itemRenderer = value;
+            invalidateComponentLayout();
+        }
+
         return value;
     }
 
@@ -64,5 +85,88 @@ class Box extends Component {
         if (style.layout != null) {
             layoutName = style.layout;
         }
+    }
+}
+
+//***********************************************************************************************************
+// Behaviours
+//***********************************************************************************************************
+@:dox(hide) @:noCompletion
+private class DataSourceBehaviour extends DataBehaviour {
+    private var _box:Box;
+
+    public function new(box:Box) {
+        super(box);
+        _box = box;
+    }
+
+    public override function set(value:Variant) {
+        super.set(value);
+        var dataSource:DataSource<Dynamic> = _value;
+        if (dataSource != null) {
+            dataSource.onDataSourceChange = function() {
+                syncChildren();
+            }
+        } else {
+            _box.removeAllComponents();
+        }
+        syncChildren();
+    }
+
+    public override function get():Variant {
+        if (_value == null || _value.isNull) {
+            _value = new ArrayDataSource<Dynamic>();
+            var dataSource:DataSource<Dynamic> = _value;
+            dataSource.onDataSourceChange = function() {
+                syncChildren();
+            }
+        }
+        return _value;
+    }
+
+    @:access(haxe.ui.backend.ComponentImpl)
+    private function syncChildren() {
+        var dataSource:DataSource<Dynamic> = _value;
+        if (dataSource == null) {
+            return;
+        }              
+
+        var itemRenderer:ItemRenderer = _box.itemRenderer;
+        if (itemRenderer == null) {
+            itemRenderer = _box.findComponent(ItemRenderer);
+            if (itemRenderer == null) {
+                return;
+            }
+
+            _box.itemRenderer = itemRenderer;
+            _box.itemRenderer.ready();
+            _box.itemRenderer.handleVisibility(false);
+        }
+
+        for (i in 0...dataSource.size) {
+            var item = dataSource.get(i);
+            var renderer = findRenderer(item);
+            if (renderer == null) {
+                renderer = itemRenderer.cloneComponent();
+                _box.addComponent(renderer);
+            }
+            _box.setComponentIndex(renderer, i);
+            renderer.data = item;
+        }
+
+        for (child in _component.findComponents(ItemRenderer)) {
+            if (dataSource.indexOf(child.data) == -1) {
+                _box.removeComponent(child);
+            }
+        }
+    }
+
+    private function findRenderer(data:Dynamic):ItemRenderer {
+        for (child in _component.findComponents(ItemRenderer)) {
+            if (child.data == data) {
+                return child;
+            }
+        }
+        return null;
     }
 }
