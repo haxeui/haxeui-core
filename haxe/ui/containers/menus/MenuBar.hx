9117c165b2ce3179afe0597c8b24bb096464a5a1
Ian Harrigan <ianharrigan@hotmail.com>
2024-04-06 11:45:34 +0200

    allow before_close to be cancelled at the menubar level

--- haxe/ui/containers/menus/MenuBar.hx
+++ haxe/ui/containers/menus/MenuBar.hx
@@ -232,6 +232,16 @@ private class Events extends haxe.ui.events.Events {
                     return;
                 }
             }
+            if (!force) {
+                var beforeCloseEvent = new UIEvent(UIEvent.BEFORE_CLOSE);
+                if (_currentMenu.hitTest(Screen.instance.currentMouseX, Screen.instance.currentMouseY)) {
+                    beforeCloseEvent.relatedComponent = _currentMenu.findComponentsUnderPoint(Screen.instance.currentMouseX, Screen.instance.currentMouseY, MenuItem)[0];
+                }
+                _menubar.dispatch(beforeCloseEvent);
+                if (beforeCloseEvent.canceled) {
+                    return;
+                }
+            }
             
             var menuEvent = new MenuEvent(MenuEvent.MENU_CLOSED);
             menuEvent.menu = _currentMenu;
