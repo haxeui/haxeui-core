1cba7e5f24c8219cf3e8687e8fa3b04f7de399f7
Ian Harrigan <ianharrigan@hotmail.com>
2024-10-07 11:29:45 +0200

    remove trace

--- haxe/ui/containers/Stack.hx
+++ haxe/ui/containers/Stack.hx
@@ -132,7 +132,6 @@ private class Builder extends CompositeBuilder {
             _stack.selectedIndex = 0;
         }
         var childIndex = _stack.getComponentIndex(child);
-        trace(childIndex, _stack.selectedIndex);
         if (childIndex != _stack.selectedIndex) {
             child.hide();
         } else if (childIndex == _stack.selectedIndex) {
