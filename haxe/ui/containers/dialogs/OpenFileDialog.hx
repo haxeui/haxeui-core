f703a138cffa7881e32d20ca59788285ad00cc5e
Ian Harrigan <ianharrigan@hotmail.com>
2024-12-21 08:38:07 +0100

    haxeui_expose_all

--- haxe/ui/containers/dialogs/OpenFileDialog.hx
+++ haxe/ui/containers/dialogs/OpenFileDialog.hx
@@ -2,5 +2,8 @@ package haxe.ui.containers.dialogs;
 
 import haxe.ui.backend.OpenFileDialogImpl;
 
+#if haxeui_expose_all
+@:expose
+#end
 class OpenFileDialog extends OpenFileDialogImpl {
 }
--- haxe/ui/containers/dialogs/SaveFileDialog.hx
+++ haxe/ui/containers/dialogs/SaveFileDialog.hx
@@ -2,5 +2,8 @@ package haxe.ui.containers.dialogs;
 
 import haxe.ui.backend.SaveFileDialogImpl;
 
+#if haxeui_expose_all
+@:expose
+#end
 class SaveFileDialog extends SaveFileDialogImpl {
 }
